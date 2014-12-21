14c9d9b615116ba21bc19bfb7c19ff8f8066e2ed
Eric Dobbertin <eric@dairystatedesigns.com>
2014-12-21 15:19:57 -0600

    bump version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'aldeed:autoform-bs-datetimepicker',
   summary: 'Custom bootstrap-datetimepicker input type with timezone support for AutoForm',
-  version: '1.0.1',
+  version: '1.0.2',
   git: 'https://github.com/aldeed/meteor-autoform-bs-datetimepicker.git'
 });
 

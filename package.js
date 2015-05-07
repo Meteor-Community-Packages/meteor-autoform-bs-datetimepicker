4b7e74ae137b12b4600cac5bb4312c6a70a63554
Eric Dobbertin <eric@dairystatedesigns.com>
2015-05-07 08:55:55 -0500

    add weak dependencies on moment packages

--- package.js
+++ package.js
@@ -9,6 +9,11 @@ Package.onUse(function(api) {
   api.use('templating@1.0.0');
   api.use('blaze@2.0.0');
   api.use('aldeed:autoform@4.0.0 || 5.0.0');
+
+  // Ensure momentjs packages load before this one if used
+  api.use('momentjs:moment@2.8.4', 'client', {weak: true});
+  api.use('mrt:moment-timezone@0.2.1', 'client', {weak: true});
+
   api.addFiles([
     'autoform-bs-datetimepicker.html',
     'autoform-bs-datetimepicker.js'

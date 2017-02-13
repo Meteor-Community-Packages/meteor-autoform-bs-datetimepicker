4cfc6290baae18b0b8d554bc5f11bf11e0b61f98
Eric Dobbertin <eric@dairystatedesigns.com>
2017-02-12 19:42:18 -0600

    Add autoform 6

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 Package.describe({
   name: 'aldeed:autoform-bs-datetimepicker',
   summary: 'Custom bootstrap-datetimepicker input type with timezone support for AutoForm',
-  version: '1.0.6',
+  version: '1.0.7',
   git: 'https://github.com/aldeed/meteor-autoform-bs-datetimepicker.git'
 });
 
 Package.onUse(function(api) {
   api.use('templating@1.0.0');
   api.use('blaze@2.0.0');
-  api.use('aldeed:autoform@4.0.0 || 5.0.0');
+  api.use('aldeed:autoform@4.0.0 || 5.0.0 || 6.0.0');
 
   // Ensure momentjs packages load before this one if used
   api.use('momentjs:moment@2.8.4', 'client', {weak: true});
